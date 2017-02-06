92be01c7ec003e5d22530aceeb1d0d2992cb4eaa
sumin <tn841@naver.com>
2017-02-06 10:10:31 +0900

    아두이노 최초 커밋

---
+++ test/.ino.cpp
@@ -0,0 +1,16 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2017-02-02 12:12:34
+
+#include "Arduino.h"
+#include "Arduino.h"
+void setup() ;
+void traverse(int dx, int dy, int dz)     ;
+void loop() ;
+
+#include "test.ino"
+
+
+#endif
